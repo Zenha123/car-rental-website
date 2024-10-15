60947e7560f339c113bc4158f355a9d4b297c2c4
Zenha123 <fathimazenha21@gmail.com>
2024-10-15 08:04:17 +0530

    commit1

---
+++ phone.js
@@ -0,0 +1,24 @@
+function toggleDropdown() {
+    // Get the dropdown content and arrow elements
+    var content = document.querySelector('.dropdown-content');
+    var arrow = document.querySelector('.arrow');
+
+    // Toggle visibility of the dropdown content
+    if (content.style.display === 'none' || content.style.display === '') {
+        content.style.display = 'block';
+        arrow.classList.add('rotate'); // Rotate the arrow when open
+    } else {
+        content.style.display = 'none';
+        arrow.classList.remove('rotate'); 
+    }
+}
+/********* */
+
+    function toggleFilter() {
+        const filterSection = document.getElementById("filter-section");
+        if (filterSection.style.display === "none" || filterSection.style.display === "") {
+            filterSection.style.display = "block";
+        } else {
+            filterSection.style.display = "none";
+        }
+    }
